40ef5c4ff3b6ec8515ea6753bbcc315076239b02
gitgiggy <106620579+gitgiggy@users.noreply.github.com>
2022-11-13 22:44:35 -0600

    Update services.vue

--- pages/services.vue
+++ pages/services.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
     <p>
-      Solidan provides consulting for PeopleSoft Campus Solutions, Customer
-      Relationship Management, Human Capital Management,and Financial
-      Management. Our services provide both leadership and full implementation
-      teams to augmentation resources on existing project teams.&nbsp;
+     
+      Slate Implementation, Optimization and Troubleshooting Support
+
+  <p>Implementation</p>
+Slate is what you want it to be but getting started is overwhelming. Our consultants leverage decades of higher education experience with their business process knowledge and technical expertise to help guide, build and train your team on Slate.  Don’t hold on to old practices. Let us work side by side with you to help you configure Slate the best way for your college.
+
+  <p>Optimization via the Solidan Slate Health Check</p>
+A module based in depth review and analysis of current configurations and processes. Modules included are: application, deliver, events, forms, reader/workflows, query/reports, rules & automations and permissions. Followed by recommendations and/or technical support fill existing gap or to move forward new processes.
+
+  <p>Troubleshooting Support</p>
+We understand resources can run thin and projects are horizon because of competing priorities. Or manual work is the workaround because the needed troubleshooting support is not available.   We offer general troubleshooting from experts with specialized knowledge who then provide workable solutions. 
+
+  <p>PeopleSoft Campus Solutions </p>
+We have been implementing, upgrading, and supporting campus solutions since 7.x through 9.2.
+
+
+  <p>Integrations</p>
+We were taught to share in kindergarten.  Your student information systems need to share as well.  We make sure data exchanges are seamless and secure.
+
+
+
     </p>
     <p>Custom Application Development</p>
     <p>Functional Business Process Consulting</p>
