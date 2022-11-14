2edaff14c16169b33f5aba3ffcf7c86a1f32c031
gitgiggy <106620579+gitgiggy@users.noreply.github.com>
2022-11-13 22:47:18 -0600

    Update services.vue

--- pages/services.vue
+++ pages/services.vue
@@ -1,25 +1,24 @@
 <template>
   <div>
-    <p>
-     
-      Slate Implementation, Optimization and Troubleshooting Support
+    <p><b>    
+      Slate Implementation, Optimization and Troubleshooting Support</b>
 
-  <p>Implementation</p>
+  <p><b>Implementation</b>
 Slate is what you want it to be but getting started is overwhelming. Our consultants leverage decades of higher education experience with their business process knowledge and technical expertise to help guide, build and train your team on Slate.  Don’t hold on to old practices. Let us work side by side with you to help you configure Slate the best way for your college.
-
-  <p>Optimization via the Solidan Slate Health Check</p>
+</p>
+  <p><b>Optimization via the Solidan Slate Health Check</b>
 A module based in depth review and analysis of current configurations and processes. Modules included are: application, deliver, events, forms, reader/workflows, query/reports, rules & automations and permissions. Followed by recommendations and/or technical support fill existing gap or to move forward new processes.
-
-  <p>Troubleshooting Support</p>
+</p>
+  <p><b>Troubleshooting Support</b>
 We understand resources can run thin and projects are horizon because of competing priorities. Or manual work is the workaround because the needed troubleshooting support is not available.   We offer general troubleshooting from experts with specialized knowledge who then provide workable solutions. 
-
-  <p>PeopleSoft Campus Solutions </p>
+</p>
+  <p><b>PeopleSoft Campus Solutions </b>
 We have been implementing, upgrading, and supporting campus solutions since 7.x through 9.2.
+</p>
 
-
-  <p>Integrations</p>
+  <p><b>Integrations</b>
 We were taught to share in kindergarten.  Your student information systems need to share as well.  We make sure data exchanges are seamless and secure.
-
+</p>
 
 
     </p>
