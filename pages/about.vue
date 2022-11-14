3ac8d9d67f2df28cda6b80f5aea968afc8a20e6e
gitgiggy <106620579+gitgiggy@users.noreply.github.com>
2022-11-13 23:01:38 -0600

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@
     <p>
       Solidan started in 2005 when it identified a gap between the consulting industry and the lack of understanding 
       of Universities and Colleges.  PeopleSoft was the main SIS software leader.  Recruitment Plus was the leading
-      admissions software.  We started a quest to learn and engage all areas of an istitution.  
+      admissions software.  We started a quest to learn and engage all areas of an institution.  
     </p>
   </div>
 </template>
