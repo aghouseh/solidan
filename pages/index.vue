45bc153f57ced90baf00583ac0120cae64f8cd4a
gitgiggy <106620579+gitgiggy@users.noreply.github.com>
2022-11-13 22:35:44 -0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,16 @@
     </h3>
     <p><img src="~/assets/images/student.jpg" alt="student.jpg" /></p>
     <p>
-      Solidan will focus on moving forward and achieving academic and
-      operational excellence for your academic institution. There is no need to
-      worry about how we ended up here; as long as, we understand how we end up
-      where we want to be. Our mission is to ensure your systems in higher
-      education are seamless. With years of satisfied clients, we have
-      established a continued relationship with the greater Higher Education
-      Industry.
+    Solidan, is a higher education consulting company that delivers expert information technology and integration support provided 
+    by true Enrollment Management practitioners.  We have over 60+ years of combined experience in admissions, financial aid, marketing,
+    analytics, training, integration, and IT support.   We have been in your shoes.  
+    
+    We specialize in providing consulting for Technolution’s Slate, PeopleSoft Campus Solutions, and system integrations.   Our services provide 
+    both leadership and full implementation teams posed to augment resources.  Solidan focuses on moving forward and achieving strategic and operational excellence 
+    for your academic institution. Our mission is to ensure your systems are seamless and working for you. With years of satisfied clients,
+    we have established a continued relationship with the greater Higher Education Industry.
+
+
     </p>
   </div>
 </template>
