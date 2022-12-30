1d39a578e30a2fbda3334165ad031ace7e338949
Andrew Householder <aghouseh@me.com>
2022-12-30 17:24:08 -0600

    added flickity dep & images

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,36 @@
 <template>
   <div>
     <h3>
-    Solidan, is a higher education consulting company that delivers expert information technology and integration support provided 
-    by true Enrollment Management practitioners.  We have over 60+ years of combined experience in admissions, financial aid, marketing,
-    analytics, training, integration, and IT support.   We have been in your shoes.  
+      Solidan, is a higher education consulting company that delivers expert
+      information technology and integration support provided by true Enrollment
+      Management practitioners. We have over 60+ years of combined experience in
+      admissions, financial aid, marketing, analytics, training, integration,
+      and IT support. We have been in your shoes.
     </h3>
-    <p><img src="~/assets/images/student.jpg" alt="student.jpg" /></p>
+    <Flickity>
+      <img src="~/assets/images/student.jpg" alt="Medical student" />
+    </Flickity>
     <p>
-  
-    
-    We specialize in providing consulting for Technolution’s Slate, PeopleSoft Campus Solutions, and system integrations.   Our services provide 
-    both leadership and full implementation teams posed to augment resources.  Solidan focuses on moving forward and achieving strategic and operational excellence 
-    for your academic institution. Our mission is to ensure your systems are seamless and working for you. With years of satisfied clients,
-    we have established a continued relationship with the greater Higher Education Industry.
-
-
+      We specialize in providing consulting for Technolution's Slate, PeopleSoft
+      Campus Solutions, and system integrations. Our services provide both
+      leadership and full implementation teams posed to augment resources.
+      Solidan focuses on moving forward and achieving strategic and operational
+      excellence for your academic institution. Our mission is to ensure your
+      systems are seamless and working for you. With years of satisfied clients,
+      we have established a continued relationship with the greater Higher
+      Education Industry.
     </p>
   </div>
 </template>
 
-<script></script>
+<script>
+import Flickity from 'vue-flickity'
+export default {
+  name: 'Home',
+  components: {
+    Flickity,
+  },
+}
+</script>
 
 <style></style>
