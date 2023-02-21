3acf0094a0afe5d1af6f7f66506c08ecf51118c0
gitgiggy <106620579+gitgiggy@users.noreply.github.com>
2023-02-21 09:46:18 -0600

    Update clients.vue

--- pages/clients.vue
+++ pages/clients.vue
@@ -40,10 +40,7 @@ export default {
           name: 'City Colleges of Chicago',
           logo: 'logo_coe_vertical.png',
         },
-         {
-          name: 'Benedictine University',
-          logo: 'ben.png',
-        },
+        
         {
           name: 'UMASS',
           logo: 'UMASS-logo-NEW.jpg',
